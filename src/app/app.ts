d2b0c54daa1a2ad2526c69036c619eb5622bfc36
GuerreroWelinton <guerrerozamora213@gmail.com>
2025-10-22 00:39:40 -0500

    feat: add footer component and update related files

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,17 +1,18 @@
 import { Component } from '@angular/core';
 
+import { BookFilters } from './components/book-filters/book-filters';
 import { BookGrid } from './components/book-grid/book-grid';
 import { Cart } from './components/cart/cart';
 import { Faqs } from './components/faqs/faqs';
+import { Footer } from './components/footer/footer';
 import { Header } from './components/header/header';
 import { Hero } from './components/hero/hero';
 import { Modal } from './components/modal/modal';
 import { Sidebar } from './components/sidebar/sidebar';
-import { BookFilters } from './components/book-filters/book-filters';
 
 @Component({
   selector: 'app-root',
-  imports: [Header, Hero, BookFilters, BookGrid, Cart, Modal, Sidebar, Faqs],
+  imports: [Header, Hero, BookFilters, BookGrid, Cart, Modal, Sidebar, Faqs, Footer],
   templateUrl: './app.html',
   styleUrl: './app.css',
 })
---
+++ src/app/components/footer/footer.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'nexus-footer',
+  imports: [],
+  templateUrl: './footer.html',
+  styleUrl: './footer.css',
+})
+export class Footer {}
